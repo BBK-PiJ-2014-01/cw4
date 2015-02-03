c89a40ab1580b459ca2a096fbcef7271a6f95120
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-02-03 03:07:32 +0000

    Tests added for the Contract Interface implementation

---
+++ TestSuite/ContactTest.java
@@ -0,0 +1,38 @@
+/**
+ * Created by PierreM on 03/02/2015.
+ * Testing the implementation of Interface Contact
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class ContactTest {
+    ContactImpl contact;
+
+    @Before
+    public void buildUp() {
+        contact = new ContactImpl(100, "Mark Holt");
+    }
+
+    @Test
+    public void tests_getID_returns_ContactID() {
+        int expected = 100;
+        assertEquals(expected, contact.getID());
+    }
+
+    @Test
+    public void tests_getName_returns_ContactName() {
+        String expected = "Mark Holt";
+        assertEquals(expected, contact.getName());
+    }
+
+    @Test
+    public void tests_getNotes_returns_NULL_ifNoNotes_returnsNotes_ifNotesAdded() {
+        assertNull("Returned value should be Null",contact.getNotes());
+        String expected = "He is the TTL CTO";
+        contact.addNotes(expected);
+        assertEquals("Expected and actual notes do not match", expected, contact.getNotes());
+    }
+}
