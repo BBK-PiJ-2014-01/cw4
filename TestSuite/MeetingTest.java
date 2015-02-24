6e6f2adb2ac44f5316a7af642b356171ef55447a
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-02-24 17:47:36 +0000

    Tests for Meeting Interface implementation

--- TestSuite/MeetingTest.java
+++ TestSuite/MeetingTest.java
@@ -5,20 +5,40 @@
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class MeetingTest {
     MeetingImpl meeting;
+    //Set contactSet;
 
     @Before
     public void buildUp() {
-        contact = new ContactImpl(100, "Pierre Meyer");
+        Set contactSet = new LinkedHashSet<ContactImpl>();
+        meeting = new MeetingImpl(1,new GregorianCalendar(2015,02,28),contactSet.add(new ContactImpl(7, "Pierre Meyer")));
     }
 
     @Test
-    public void tests_getID_ReturnsContactID() {
-        int expected = 100;
-        assertEquals(expected, contact.getId());
+    public void tests_getID_ReturnsMeetingID() {
+        int expected = 1;
+        assertEquals(expected, meeting.getId());
+    }
+
+    @Test
+    public void tests_getDate_ReturnsMeetingDate() {
+        Calendar expected = new GregorianCalendar(2015,02,28);
+        assertEquals(expected, meeting.getDate());
+    }
+
+    @Test
+    public void tests_getContacts_ReturnsDetailsOfPeopleAttendingMeeting() {
+        Set expected = contactSet;
+        assertEquals(expected, meeting.getContacts());
     }
 }
