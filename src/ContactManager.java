dc2a2c4a8e3b1b9fdb067c001e70e3b9879a3eeb
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-02-28 17:58:26 +0000

    Adding addNewContact() method definition in ContactManager Interface

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -2,4 +2,14 @@
  * Created by PierreM on 02/02/2015.
  */
 public interface ContactManager {
+
+    /**
+     * Create a new contact with the specified name and notes.
+     *
+     * @param name the name of the contact.
+     * @param notes notes to be added about the contact.
+     * @throws NullPointerException if the name or the notes are null
+     */
+    void addNewContact(String name, String notes);
+    
 }
---
+++ src/ContactManagerLauncher.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Pierre on 28/02/2015
+ * Application launcher
+ */
+public class ContactManagerLauncher {
+    public static void main(String[] args) {
+        ContactManagerLauncher launcher = new ContactManagerLauncher();
+        launcher.run();
+    }
+
+    public void run() {
+
+    }
+}
