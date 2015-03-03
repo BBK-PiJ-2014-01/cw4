e287a86de23c2045ecbcd09b5688f78551570f15
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-03-03 23:34:30 +0000

    Implementing tests for getContacts(int.. ids) method - testing the output list of contacts is correct - testing the IllegalArgumentException is thrown when invalid ID as parameter Implementing tests for getContacts(String name) method - testing the output list of contacts is correct - testing the NullPointerException is thrown when null parameter

--- TestSuite/ContactManagerContactsRelatedTest.java
+++ TestSuite/ContactManagerContactsRelatedTest.java
@@ -5,6 +5,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 public class ContactManagerContactsRelatedTest {
@@ -33,4 +36,57 @@ public class ContactManagerContactsRelatedTest {
     public void tests_AddNewContact_ThrowsNullPointerExceptionIfNotesAreNull() {
         contactManager.addNewContact("Pierre Meyer",null);
     }
+
+    @Test
+    public void tests_getContacts_ReturnsContactsCorrespondingToRequestedIDs() {
+        Contact c1 = new ContactImpl("Joe Blogg", "Estate agent");
+        Contact c2 = new ContactImpl("Keith Smith","Grocer");
+        Contact c3 = new ContactImpl("Bob Builder","Dentist");
+        Contact c4 = new ContactImpl("Dave Moon","Pharmacist");
+        Contact c5 = new ContactImpl("Barry White","Singer");
+        contactManager.addNewContact(c1);
+        contactManager.addNewContact(c2);
+        contactManager.addNewContact(c3);
+        contactManager.addNewContact(c4);
+        contactManager.addNewContact(c5);
+
+        Set<Contact> expectedContactOutputList = new LinkedHashSet<Contact>();
+        expectedContactOutputList.add(c1);
+        expectedContactOutputList.add(c3);
+        expectedContactOutputList.add(c4);
+
+        assertEquals("Output list does not include required contacts",expectedContactOutputList,getContacts(1,3,4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tests_getContacts_ThrowsIllegalArgumentsExceptionIfInvalidID() {
+        contactManager.addNewContact("Pierre Meyer","Student");
+        getContacts(2);
+    }
+
+    @Test
+    public void tests_getContacts_ReturnsContactsWhoseNameContainsRequiredString() {
+        Contact c1 = new ContactImpl("Joe Blogger", "Estate agent");
+        Contact c2 = new ContactImpl("Keith Blogg","Grocer");
+        Contact c3 = new ContactImpl("Bob Builder","Dentist");
+        Contact c4 = new ContactImpl("Dave Moon","Pharmacist");
+        Contact c5 = new ContactImpl("Bloggy Bull","Singer");
+        contactManager.addNewContact(c1);
+        contactManager.addNewContact(c2);
+        contactManager.addNewContact(c3);
+        contactManager.addNewContact(c4);
+        contactManager.addNewContact(c5);
+
+        Set<Contact> expectedContactOutputList = new LinkedHashSet<Contact>();
+        expectedContactOutputList.add(c1);
+        expectedContactOutputList.add(c2);
+        expectedContactOutputList.add(c5);
+
+        assertEquals("Output list does not include required contacts",expectedContactOutputList,getContacts("Blog"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void tests_getContacts_ThrowsNullPointerExceptionIfNullParameter() {
+        contactManager.addNewContact("Pierre Meyer","Student");
+        getContacts(null);
 }
