01c6ff34d6238a58d88d4109b8a7ff0277afa1cf
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-03-03 00:20:57 +0000

    Adding Set<Contact> getContacts(int.. ids) method definition in ContactManager interface Adding Set<Contact> getContacts(String name) method definition in ContactManager interface

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 /**
  * Created by PierreM on 02/02/2015.
  */
@@ -13,4 +15,21 @@ public interface ContactManager {
      */
     void addNewContact(String name, String notes);
 
+    /**
+     * Returns a list containing the contacts that correspond to the IDs.
+     *
+     * @param ids an arbitrary number of contact IDs
+     * @return a list containing the contacts that correspond to the IDs.
+     * @throws IllegalArgumentException if any of the IDs does not correspond to a real contact
+     */
+    Set<Contact> getContacts(int... ids);
+
+    /**
+     * Returns a list with the contacts whose name contains that string.
+     *
+     * @param name the string to search for
+     * @return a list with the contacts whose name contains that string.
+     * @throws NullPointerException if the parameter is null
+     */
+    Set<Contact> getContacts(String name);
 }
