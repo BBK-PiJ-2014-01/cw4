9c07f2fa8dd752bc5b349238994fa41a34ae8c6d
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-03-08 16:05:14 +0000

    Creation of Tests for AddFutureMeeting method - Check the meeting is added to the ContactManager meetingSet - Check the date of a new meeting is not in teh past - Check that the contact is known/existent in the ContactManager contactSet

---
+++ TestSuite/ContactManagerFutureMeetingsRelatedTest.java
@@ -0,0 +1,52 @@
+/**
+ * Created by Pierre on 07/03/2015.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ContactManagerFutureMeetingsRelatedTest {
+
+    ContactManagerImpl contactManager ;
+    Contact contact1;
+    Contact contact2;
+    Contact contact3;
+    Set<Contact> myContactSet;
+
+    @Before
+    public void buildUp() {
+        contactManager = new ContactManagerImpl();
+        contact1 = new ContactImpl("Amelie Worth");
+        contact2 = new ContactImpl("Brian Sprout");
+        contact3 = new ContactImpl("Julie Miller");
+        contactManager.addNewContact(contact1);
+        contactManager.addNewContact(contact2);
+        myContactSet = contactManager.getContacts(1,2);
+    }
+
+    @Test
+    public void tests_addFutureMeeting_IsAddedToMeetingSet() {
+        int expected = 0;
+        assertEquals("meetingSet should be empty",expected,contactManager.getMeetingSet().size());
+        contactManager.addFutureMeeting(myContactSet, new GregorianCalendar(2015, 12, 20);
+        expected++;
+        assertEquals("meetingSet should have 1 element",expected,contactManager.getMeetingSet().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tests_AddFutureMeeting_ThrowsIllegalArgumentExceptionIfDateIsInThePast() {
+        contactManager.addFutureMeeting(myContactSet, new GregorianCalendar(2015,02,28));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void tests_AddFutureMeeting_ThrowsIllegalArgumentExceptionIfContactIsUnknown() {
+        myContactSet.add(contact3);
+        contactManager.addFutureMeeting(myContactSet, new GregorianCalendar(2015,12,20));
+    }
+
+}
